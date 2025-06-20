c3f473899e85eda97ef44d41e46a701c0bd0ea00
john turner <7strbass@gmail.com>
2025-06-20 15:29:18 -0400

    --use most recent index instead of size of map for next button indices.

--- src/SOM_GeometryProj_PKG/geom_UI/Geom_SphereAnimResWin.java
+++ src/SOM_GeometryProj_PKG/geom_UI/Geom_SphereAnimResWin.java
@@ -85,7 +85,7 @@ public class Geom_SphereAnimResWin extends SOM_AnimWorldWin {
 		}
 	}//setPrivFlags_Indiv
 	/**
-	 * Build all UI objects to be shown in left side bar menu for this window.  This is the first child class function called by initThisWin
+	 * Build all UI objects to be shown in left side bar menu for this window. This is the first child class function called by initThisWin
 	 * @param tmpUIObjMap : map of GUIObj_Params, keyed by unique string, with values describing the UI object
 	 * 			- The object IDX                   
 	 *          - A double array of min/max/mod values                                                   
@@ -96,11 +96,15 @@ public class Geom_SphereAnimResWin extends SOM_AnimWorldWin {
 	 *           	idx 0: value is sent to owning window,  
 	 *           	idx 1: value is sent on any modifications (while being modified, not just on release), 
 	 *           	idx 2: changes to value must be explicitly sent to consumer (are not automatically sent),
-	 *          - A boolean array of renderer format values :(unspecified values default to false)
-	 *           	idx 0: whether multi-line(stacked) or not                                                  
-	 *              idx 1: if true, build prefix ornament                                                      
-	 *              idx 2: if true and prefix ornament is built, make it the same color as the text fill color.
+	 *          - A boolean array of renderer format values :(unspecified values default to false) - Behavior Boolean array must also be provided!
+	 * 				idx 0 : Should be multiline
+	 * 				idx 1 : One object per row in UI space (i.e. default for multi-line and btn objects is false, single line non-buttons is true)
+	 * 				idx 2 : Text should be centered (default is false)
+	 * 				idx 3 : Object should be rendered with outline (default for btns is true, for non-buttons is false)
+	 * 				idx 4 : Should have ornament
+	 * 				idx 5 : Ornament color should match label color r
 	 */
+	@Override
 	protected final void setupGUIObjsAras_Indiv(TreeMap<String, GUIObj_Params> tmpUIObjMap) {
 		tmpUIObjMap.put("gIDX_MinRadius", uiMgr.uiObjInitAra_Float(gIDX_MinRadius, new double[]{20,500,1}, (double)minSphRad, "Min sphere radius"));   				//gIDX_MinRadius	                                                                        
 		tmpUIObjMap.put("gIDX_MaxRadius", uiMgr.uiObjInitAra_Float(gIDX_MaxRadius, new double[]{100,1000,1},(double)maxSphRad, "Max sphere radius"));  				//gIDX_MaxRadius	                                                                        	
@@ -108,14 +112,15 @@ public class Geom_SphereAnimResWin extends SOM_AnimWorldWin {
 
 	/**
 	 * Build all UI buttons to be shown in left side bar menu for this window. This is for instancing windows to add to button region
-	 * USE tmpUIBoolSwitchObjMap.size() for start idx
+	 * @param firstIdx : the first index to use in the map/as the objIdx
 	 * @param tmpUIBoolSwitchObjMap : map of GUIObj_Params to be built containing all flag-backed boolean switch definitions, keyed by sequential value == objId
 	 * 				the first element is the object index
 	 * 				the second element is true label
 	 * 				the third element is false label
 	 * 				the final element is integer flag idx 
 	 */
-	protected final void setupGUIBoolSwitchAras_Indiv(TreeMap<String, GUIObj_Params> tmpUIBoolSwitchObjMap) {}
+	@Override
+	protected final void setupGUIBoolSwitchAras_Indiv(int firstIdx, TreeMap<String, GUIObj_Params> tmpUIBoolSwitchObjMap) {}
 	
 	@Override
 	protected final String[] setUI_GeomObjFeatureListVals() {	return Geom_SphereSOMExample.ftrNames;};	
